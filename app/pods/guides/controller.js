8b0f66019fca6a95520716b5c6bef3d178693bac
Charles Bido <charlesbido@me.com>
2016-04-11 15:31:12 -0500

    add search feature

---
+++ app/pods/guides/controller.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  searchQuery: '',
+
+  filteredContent: Ember.computed('model.[]', 'searchQuery', function() {
+    var filter = this.get("searchQuery");
+    var rx = new RegExp(filter, 'gi');
+    var guides = this.get('model');
+
+
+    return guides.filter(function(guide) {
+      return guide.get('title').match(rx);
+    });
+  })
+  
+});
