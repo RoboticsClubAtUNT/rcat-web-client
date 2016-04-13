7ba76d0455f643bed9b1bdd355aeb086c7623927
Charles Bido <charlesbido@me.com>
2016-04-13 14:17:40 -0500

    finish sign up form

--- app/pods/signup/controller.js
+++ app/pods/signup/controller.js
@@ -5,9 +5,32 @@ export default Ember.Controller.extend({
   actions: {
     submitForm() {
       this.set('isSubmitted', true);
-      this.$.ajax({
-        url: ""
-      })
+      Ember.$.ajax({
+        type: "POST",
+        url: (function() {
+          var url = "";
+          url += window.location.protocol + "//" + window.location.hostname
+          if (window.location.port) {
+            url += ":" + window.location.port;
+          }
+          url +=  "/api/v2/membership-requests";
+          return url;
+        })(),
+        data: {
+          "data": {
+            "type": "membershipRequest",
+            "attributes": {
+              "name": this.get("name"),
+              "euid": this.get("euid"),
+              "email": this.get("email")
+            }
+          }
+        },
+        success: function(data) {
+          console.log(data);
+        }
+
+      });
     }
   }
 
